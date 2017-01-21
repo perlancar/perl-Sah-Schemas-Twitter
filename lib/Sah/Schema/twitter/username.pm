7197b6adcbca30dee1a186c17331795023a931bc
perlancar (netbook-zenbook-ux305) <perlancar@gmail.com>
2017-01-21 17:16:45 +0700

    Initial commit

---
+++ lib/Sah/Schema/twitter/username.pm
@@ -0,0 +1,17 @@
+package Sah::Schema::twitter::username;
+
+# DATE
+# VERSION
+
+our $schema => ["cistr", {
+    summary => 'Twitter username',
+    match => '\A[0-9A-Za-z_]{1,15}\z',
+}, {}];
+
+1;
+
+# ABSTRACT: Twitter username
+
+=head1 SEE ALSO
+
+L<Regexp::Pattern::Twitter>
---
+++ lib/Sah/Schemas/Twitter.pm
@@ -0,0 +1,15 @@
+package Sah::Schemas::Twitter;
+
+1;
+# ABSTRACT: Sah schemas related to Twitter
+
+=head1 SYNOPSIS
+
+
+=head1 prepend:SEE ALSO
+
+L<https://twitter.com>
+
+L<Regexp::Pattern::Twitter>
+
+=cut
